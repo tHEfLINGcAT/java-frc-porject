58dc4599c13b12606a54f135177740179a40ef90
tHEfLINGcAT <tamirbirbraer@gmail.com>
2023-11-19 18:50:19 +0200

    chages in main that saves all the persons
    created a function called get people, which takes all the persons and saves that in a list called people, then we created a loop that if the user types quit the loop will end and it will print all the persons the user typed

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.jar.Attributes.Name;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner Scanner= new Scanner(System.in);
-        System.out.println("put the oldest parent name:");
-        
-
-    
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("put the name and age of the oldest person to the yougest person(when u want to stop, write quit):");
+        ArrayList<Person> sortedList=getPeople(scanner);
+        print(sortedList);
     }
     public static Person createPerson(Scanner scanner){
-        String name=scanner.nextLine();
+        System.out.println("enter name:");
+        String name=scanner.next();
+        System.out.println("enter age:");
         int age=scanner.nextInt();
-
         while (!(age>=0 && age<=120)) {
             System.out.println("type a number between 0 and 120");
             age=scanner.nextInt();
         }
 
         return new Person(age, name);
-    } 
+    }
+    public static ArrayList<Person> getPeople(Scanner scanner){
+        ArrayList<Person> people=new ArrayList<Person>();
+        do{
+            people.add(createPerson(scanner));
+
+        }while(!people.get(people.size()-1).getName().equals("quit"));
+        people.remove(people.get(people.size()-1));
+        people.sort((Person p1,Person p2)->p1.getAge()-p2.getAge());
+
+        return people;
+    }
+    public static void print(ArrayList<Person> arrlist){
+        Iterator<Person> itr=arrlist.iterator();
+        while(itr.hasNext())
+          {
+            Person obj = itr.next();
+            System.out.println(obj);
+          }
+    }
 }
+
  
--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class Person {
-    private String name;
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + "]";
+    }
+
+    public String name;
     private int age;
      
 
